ee45d23458a74c4df36b134a2d09c5669eef0097
Bryce <brycecortez1337@gmil.com>
2022-11-15 16:22:24 -0800

    green color

--- scripts/script.js
+++ scripts/script.js
@@ -30,13 +30,61 @@
 // greet("Bryce" , "Night", );
 
 
-let r = prompt("enter a radius. ");
-console.log(r)
-function circleArea (radius)
+// let r = prompt("enter a radius. ");
+// console.log(r)
+// function circleArea (radius)
+// {
+//     const radiusP = document.querySelector("#radius");
+//     const resultP = document.querySelector("#result");
+// radiusP.textContent = radius;  
+// let calculatedArea = Math.PI * radius * radius;
+// resultP.textContent = calculatedArea
+// return calculatedArea;
+
+    
+// }
+
+// let area = circleArea(r).toFixed(2);
+
+// alert(`The area of a circle with radius ${r} is ${area}`)
+
+const shoppingList = ["eggs", "milk", "bread", "green tea"];
+const shopul = document.querySelector(".shopping")
+function shop (shopping)
 {
-    return Math.PI * radius * radius;
+   for (let i = 0; i < shopping.length; i++)
+   {
+    let newLi = document.createElement("li");
+    newLi.textContent = shopping[i];
+    shopul.append(newLi);
+    console.log(shopping[i]);
+   }
 }
 
-let area = circleArea(r).toFixed(2);
 
-alert(`The area of a circle with radius ${r} is ${area}`)
+shop(shoppingList)
+
+function changeListStyle()
+{
+shopul.classList.remove("cricleList");
+shopul.classList.add ("squareList");
+
+
+}
+changeListStyle()
+
+const listItems = document.querySelectorAll(".shoppping li")
+
+function greenItem()
+{
+    for (let i = 0 ; i<greenItem.length; i++)
+    {
+     if ( listItems[i].textContent.includes("green") )
+     {
+        listItems[i].classList.add ("green");
+
+     }
+    }
+}
+
+greenItem();
